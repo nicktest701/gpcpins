e29576d1a934b08dc6027cc4a2395c1da3abffb5
nicktest701 <nicktest701@gmail.com>
2025-10-16 12:30:20 +0000

    err

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -1,42 +1,45 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEMail = async (email_address, message, subject) => {
-
-  if (process.env.NODE_ENV !== 'production') return true
+  // if (process.env.NODE_ENV !== 'production') return true
 
   try {
     const transportMail = nodemailer.createTransport({
-      // service: process.env.MAIL_CLIENT_SERVICE,
       host: process.env.MAIL_CLIENT_SERVICE,
       auth: {
         user: process.env.MAIL_CLIENT_USER,
         pass: process.env.MAIL_CLIENT_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+        ciphers: "SSLv3",
+      },
       port: 465,
       secure: true,
       from: process.env.MAIL_CLIENT_USER,
+      connectionTimeout: 10000,
       tls: {
         rejectUnauthorized: false,
       },
     });
 
     // verify connection configuration
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== "production") {
       transportMail.verify(function (error, success) {
         if (error) {
-          console.log('Err is', error);
+          console.log("Err is", error);
         } else {
-          console.log('Server is ready to take our messages');
+          console.log("Server is ready to take our messages");
         }
       });
     }
-    
+
     const mailOptions = {
       from: `GPC ${process.env.MAIL_CLIENT_USER}`,
       sender: process.env.MAIL_CLIENT_USER,
-      to: typeof email_address === 'string' ? [email_address] : email_address,
-      subject: subject || 'Gab Powerful Consult',
-      text: '',
+      to: typeof email_address === "string" ? [email_address] : email_address,
+      subject: subject || "Gab Powerful Consult",
+      text: "",
       html: message,
     };
 
