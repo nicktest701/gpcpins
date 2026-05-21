d206953ad1d0d78d24053cbf30634af3a91be5b7
nicktest701 <nicktest701@gmail.com>
2026-05-21 18:50:51 +0000

    fix redis url

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,39 +1,36 @@
 // redisClient.js
-const {Redis} = require("@upstash/redis");
+const redis = require("redis");
+const { Redis } = require("@upstash/redis");
+let redisClient;
 
-const redisClient = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+if (process.env.NODE_ENV === "production") {
+  const redisClient = redis.createClient({
+    url: process.env.REDIS_HOST,
+    // password: process.env.REDIS_PASSWORD,
+  });
 
+  redisClient.on("error", (err) => {
+    if (process.env.NODE_ENV !== "production") {
+      console.error("Redis client error:", err);
+    }
+    throw err;
+  });
+
+  (async () => {
+    try {
+      await redisClient.connect();
+    } catch (error) {
+      if (process.env.NODE_ENV !== "production") {
+        console.error("Redis client error:", error);
+      }
+      throw error;
+    }
+  })();
+} else {
+  redisClient = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  });
+}
 
 module.exports = redisClient;
-
-
-// // redisClient.js
-// const redis = require("redis");
-
-// const redisClient = redis.createClient({
-//   url: process.env.UPSTASH_REDIS_REST_URL,
-//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
-// });
-
-// redisClient.on("error", (err) => {
-//   if (process.env.NODE_ENV !== "production") {
-//     console.error("Redis client error:", err);
-//   }
-//   throw err;
-// });
-
-// (async () => {
-//   try {
-//     await redisClient.connect();
-//   } catch (error) {
-//     if (process.env.NODE_ENV !== "production") {
-//       console.error("Redis client error:", error);
-//     }
-//     throw error;
-//   }
-// })();
-
-// module.exports = redisClient;
